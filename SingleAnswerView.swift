afffb5c6c7dbbec5bb371c5785be98e56ac91d6b
YumiMachino <y.y.1515m@gmail.com>
2020-12-27 17:06:48 -0800

    initial commit

---
+++ SingleAnswerView.swift
@@ -0,0 +1,20 @@
+//
+//  SingleAnswerView.swift
+//  Unit3_PersonalityQuiz
+//
+//  Created by Yumi Machino on 2020/12/27.
+//
+
+import UIKit
+
+class SingleAnswerView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Unit3_PersonalityQuiz/ViewControllers/QuestionViewController.swift
+++ Unit3_PersonalityQuiz/ViewControllers/QuestionViewController.swift
@@ -18,37 +18,39 @@ class QuestionViewController: UIViewController {
     var slider: UISlider = UISlider()
     var index: Int = 0
     
-    var  questions: [Question] = [
-        Question(
-            text: "Which food do you like the most?",
-            type: .Single,
-            answers: [
-                Answer(text: "Steak", type: .dog),
-                Answer(text: "Fish", type: .cat),
-                Answer(text: "Carrots", type: .rabbit),
-                Answer(text: "Corn", type: .turtle)
-            ]
-        ),
-        Question(
-            text: "Which activities do you enjoy?",
-            type: .multiple,
-            answers: [
-                Answer(text: "Swimming", type: .turtle),
-                Answer(text: "Sleeping", type: .cat),
-                Answer(text: "Cuddling", type: .rabbit),
-                Answer(text: "Eating", type: .dog)
-            ]
-        ),
-        Question(
-            text: "How much do you enjoy car rides?",
-            type: .ranged,
-            answers: [
-                Answer(text: "I dislike them", type: .cat),
-                Answer(text: "I get a little nervous", type: .rabbit),
-                Answer(text: "I barely notice them", type: .turtle),
-                Answer(text: "I love them", type: .dog)
-            ]
-        )]
+    let singleView: UIView = SingleAnswerView()
+    
+//    var  questions: [Question] = [
+//        Question(
+//            text: "Which food do you like the most?",
+//            type: .Single,
+//            answers: [
+//                Answer(text: "Steak", type: .dog),
+//                Answer(text: "Fish", type: .cat),
+//                Answer(text: "Carrots", type: .rabbit),
+//                Answer(text: "Corn", type: .turtle)
+//            ]
+//        ),
+//        Question(
+//            text: "Which activities do you enjoy?",
+//            type: .multiple,
+//            answers: [
+//                Answer(text: "Swimming", type: .turtle),
+//                Answer(text: "Sleeping", type: .cat),
+//                Answer(text: "Cuddling", type: .rabbit),
+//                Answer(text: "Eating", type: .dog)
+//            ]
+//        ),
+//        Question(
+//            text: "How much do you enjoy car rides?",
+//            type: .ranged,
+//            answers: [
+//                Answer(text: "I dislike them", type: .cat),
+//                Answer(text: "I get a little nervous", type: .rabbit),
+//                Answer(text: "I barely notice them", type: .turtle),
+//                Answer(text: "I love them", type: .dog)
+//            ]
+//        )]
     
     // MARK: - viewDidLoad
     override func viewDidLoad() {
